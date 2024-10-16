d44bbdcdcdd1cd94b3f5aea38b705ad86198409a
PippyUrkel <bruhbruhbruh238@gmail.com>
2024-10-16 07:21:21 +0530

    Added some changes to the styling and overall look

--- website/views.py
+++ website/views.py
@@ -1,14 +1,39 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, jsonify
 from website.firebase import db
 from config.firebase_config import firebase_config
 import pyrebase
+import serial
 
 views = Blueprint('views', __name__)
 
+SERIAL_PORT = '/dev/ttyACM0'  # Change to the correct port for your system
+BAUD_RATE = 9600
+
+# Open the serial connection
+try:
+    ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+except:
+    print("Serial init fail")
 
 @views.route('/')
 def home():
     return render_template("landing.html")
+
+@views.route('/status')
+def status():
+    try:
+        if ser.in_waiting > 0:
+            sensor_data = ser.readline().decode('utf-8').strip()
+            if "Object detected" in sensor_data:
+                status = "Object is near"
+            else:
+                status = "No object near"
+        else:
+            status = "No data received"
+
+        return jsonify(sensor_status=status)
+    except:
+        return jsonify(sensor_status="blank")
 
 @views.route('/add_data')
 def add_data():
@@ -21,4 +46,16 @@ def add_data():
 
 @views.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    try: 
+        if ser.in_waiting > 0:
+            sensor_data = ser.readline().decode('utf-8').strip()
+            print(sensor_data)
+            if "Object detected" in sensor_data:
+                status = "Object is near"
+            else:
+                status = "No object near"
+        else:
+            status = "No data received"
+        return render_template('dashboard.html', sensor_status = status)
+    except:
+        return render_template('dashboard.html', sensor_status = "blank")
