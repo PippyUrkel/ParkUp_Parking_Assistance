0caf3dc29e6fad16ec20970b09eaa20e7983c787
Andre Dsouza <90761232+PippyUrkel@users.noreply.github.com>
2025-03-19 23:12:43 +0530

    Updated Carstatus as well as updated Theme toggler

--- website/static/scripts/script.js
+++ website/static/scripts/script.js
@@ -2,17 +2,13 @@ function changeStatus(carId) {
     const car = document.getElementById(carId);
     const statusElement = car.querySelector('.status');
     
-    // Define the statuses to cycle through
     const statuses = ['Parked', 'Over Time Limit', 'In Servicing'];
     const statusClasses = ['parked', 'over-time', 'servicing'];
     
-    // Get the current status index
     let currentStatusIndex = statuses.indexOf(statusElement.innerText);
     
-    // Cycle to the next status
     let nextStatusIndex = (currentStatusIndex + 1) % statuses.length; // this is kinda like circular queue lol
     
-    // Update the status text and class
     statusElement.innerText = statuses[nextStatusIndex];
     statusElement.className = 'status ' + statusClasses[nextStatusIndex];
 }
